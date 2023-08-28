22e290a15027b77a0d939d474aa80a831d1beba7
Ernest <em123nest@gmail.com>
2023-08-28 01:10:42 +0100

    Second commit of the tasks

--- src/aircondition/AirCondition.java
+++ src/aircondition/AirCondition.java
@@ -2,4 +2,41 @@ package aircondition;
 
 public class AirCondition {
 
+    private boolean airConditionTurnedOn;
+    private boolean airConditionTurnedOff;
+    private int increaseAirConditionTemperature;
+    private int decreaseAirConditionTemperature;
+
+
+    public boolean isAirConditionTurnedOn() {
+        return airConditionTurnedOn;
+    }
+
+    public void setAirConditionTurnedOn(boolean airConditionTurnedOn) {
+        this.airConditionTurnedOn = airConditionTurnedOn;
+    }
+
+    public boolean isAirConditionTurnedOff() {
+        return airConditionTurnedOff;
+    }
+
+    public void setAirConditionTurnedOff(boolean airConditionTurnedOff) {
+        this.airConditionTurnedOff = airConditionTurnedOff;
+    }
+
+    public int getIncreaseAirConditionTemperature() {
+        return increaseAirConditionTemperature;
+    }
+
+    public void setIncreaseAirConditionTemperature(int increaseAirConditionTemperature) {
+        this.increaseAirConditionTemperature = increaseAirConditionTemperature;
+    }
+
+    public int getDecreaseAirConditionTemperature() {
+        return decreaseAirConditionTemperature;
+    }
+
+    public void setDecreaseAirConditionTemperature(int decreaseAirConditionTemperature) {
+        this.decreaseAirConditionTemperature = decreaseAirConditionTemperature;
+    }
 }
--- tdd/aircondition/AirConditionTest.java
+++ tdd/aircondition/AirConditionTest.java
@@ -1,10 +1,41 @@
 package aircondition;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 class AirConditionTest {
+    AirCondition test = new AirCondition();
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+
+    }
+
+    @Test
+    public void testThatTheAirConditionIsTurnedOn() {
+
+        Assertions.assertFalse(test.isAirConditionTurnedOn());
+    }
+
+    @Test
+    public void testThatTheAirConditionIsTurnedOff() {
+        Assertions.assertFalse(test.isAirConditionTurnedOn());
+
+        Assertions.assertFalse(test.isAirConditionTurnedOff(), "true");
+    }
+
+    @Test
+    public void testThatTheAirConditionCanIncreaseTemperatureInARoom() {
+        Assertions.assertFalse(test.isAirConditionTurnedOn());
+
+        test.setIncreaseAirConditionTemperature(17);
+        Assertions.assertEquals(17, test.getIncreaseAirConditionTemperature());
+    }
+
+    @Test
+    public void testThatTheAirConditionCanDecreaseTemperatureInARoom() {
+        Assertions.assertFalse(test.isAirConditionTurnedOn());
+
+        test.setDecreaseAirConditionTemperature(16);
+        Assertions.assertEquals(16, test.getDecreaseAirConditionTemperature());
     }
 }
