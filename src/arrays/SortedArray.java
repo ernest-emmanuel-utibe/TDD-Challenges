8e67e046541345aa1688898dc2a5f00eb79bea35
Ernest <em123nest@gmail.com>
2023-09-01 00:50:18 +0100

    Task completed

--- src/arrays/SortedArray.java
+++ src/arrays/SortedArray.java
@@ -1,13 +1,21 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SortedArray {
 
-    public static boolean isSorted(int[] array, int n) {
-        for(int i = 1; i < n; i++) {
-            if (array[i] < array[i - 1]) {
-                return false;
-            }
-        }
-        return true;
+    public static List<Integer> sortedArray(int[] number) {
+        List<Integer> numbers= new ArrayList<Integer>();
+        numbers.add(2);
+        numbers.add(1);
+        numbers.add(4);
+        numbers.add(3);
+
+        Collections.sort(numbers);
+
+
+        return numbers;
     }
 }
--- tdd/arrays/SortedArrayTest.java
+++ tdd/arrays/SortedArrayTest.java
@@ -1,11 +1,9 @@
 package arrays;
 
-import static arrays.SortedArray.isSorted;
-
 class SortedArrayTest {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10}, n = new int[]{5};
-        System.out.println(isSorted(array, n));
+        int[] array = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(SortedArray.sortedArray(array));
     }
 
 }
